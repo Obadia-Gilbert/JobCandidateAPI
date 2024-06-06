3c05238f02e0139cc4e5b673c94898e67507c00a
Obadia Gilbert <gilbertobadia830@gmail.com>
2024-06-06 19:37:30 +0300

    Added a Candidate Model Class Including Data Anotations Schema

---
+++ JobCandidateHubAPI/Models/Candidate.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace JobCandidateHubAPI.Models
+{
+	public class Candidate
+	{
+        [Key]
+        [Required]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string FirstName { get; set; }
+
+        [StringLength(50)]
+        public string LastName { get; set; }
+
+        [Phone]
+        public string PhoneNumber { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [EnumDataType(typeof(CallTimeInterval))]
+        public string CallTimeInterval { get; set; }
+
+        [Url]
+        public string LinkedInUrl { get; set; }
+
+        [Url]
+        public string GitHubUrl { get; set; }
+
+        [Url]
+        public string Comments { get; set; }
+        
+    }
+
+
+    public enum CallTimeInterval
+    {
+        Morning,
+        Afternoon,
+        Evening
+    }
+}
+
