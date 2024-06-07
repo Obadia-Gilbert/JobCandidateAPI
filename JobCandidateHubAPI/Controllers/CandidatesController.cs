c73d7fc3c167bd522edcf75e3fc2fdcf633e6436
Obadia Gilbert <gilbertobadia830@gmail.com>
2024-06-07 18:08:06 +0300

    Controller naming - api/candidates

--- JobCandidateHubAPI/Controllers/CandidatesController.cs
+++ JobCandidateHubAPI/Controllers/CandidatesController.cs
@@ -5,7 +5,7 @@ using Microsoft.AspNetCore.Mvc;
 namespace JobCandidateHubAPI.Controllers;
 
 [ApiController]
-[Route("[controller]")]
+[Route("api/[controller]")]
 public class CandidatesController : ControllerBase
 {
     private readonly CandidateService candidateService;
